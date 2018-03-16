d6c8da740f4aaf9d744447055379f249b3b01a20
GBTC441019UR <GBTC441019UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 14:38:01 -0400

    lesson complete

---
+++ src/main/java/com/example/demo/SpringBoot16aApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot16aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot16aApplication.class, args);
+	}
+}
